60bb1ef124715f95f4982eb3c6eb8eedc690c80c
Jonathan Ballet <jon@multani.info>
2011-08-26 12:18:00 +0900

    Better HTTP cache for the tests
    It now handles the response headers and returns more 'natural' object
    than before (ie. it should return the same objects as the real, non-caching
    API.)

--- libalbumart/tests/utils.py
+++ libalbumart/tests/utils.py
@@ -1,5 +1,73 @@
+import httplib
 import os
+import urllib
 import urllib2
+
+
+class CachingHTTPHandler(urllib2.HTTPHandler, object):
+    def __init__(self, caching_dir):
+        super(self.__class__, self).__init__()
+        self.nb_calls = 0
+        self.caching_dir = caching_dir
+
+    @property
+    def current_cache_path(self):
+        return os.path.join(self.caching_dir, "%d.cache" % self.nb_calls)
+
+    def http_open(self, request):
+        self.nb_calls += 1
+
+        if isinstance(request, basestring):
+            url = request
+        else:
+            url = request.get_full_url()
+
+        self.parent.urls_called.append(url)
+
+        if 'LIBALBUM_ART_TEST_ONLINE' in os.environ:
+            return super(self.__class__, self).http_open(request)
+
+        self.cache_request(request, self.current_cache_path)
+
+        if os.path.exists(self.current_cache_path):
+            return self.uncache_response(url, self.current_cache_path)
+        else:
+            response = super(self.__class__, self).http_open(request)
+            self.cache_response(response, self.current_cache_path)
+            return response
+
+    def cache_request(self, request, path):
+        with open("%s.request" % path, 'w') as fp:
+            fp.write('%s %s\n' % (
+                'GET' if not request.data else 'POST',
+                request.get_full_url()))
+            fp.write("Host: %s\n" % request.host)
+
+            for v in sorted(request.headers.items()):
+                fp.write("%s: %s\n" % v)
+
+            if request.data:
+                fp.write("\n%s\n" % request.data)
+
+    def cache_response(self, response, path):
+        with open("%s.headers" % path, 'w') as fp:
+            for header, value in response.headers.items():
+                fp.write("%s: %s\n" % (header, value))
+
+        fp = open(path, 'w')
+        def read():
+            data = response.old_read()
+            fp.write(data)
+            return data
+
+        response.old_read = response.read
+        response.read = read
+
+    def uncache_response(self, url, path):
+        return urllib.addinfourl(
+            open(path),
+            httplib.HTTPMessage(open(path + ".headers")),
+            url)
 
 
 class CachingOpener(urllib2.OpenerDirector):
@@ -7,51 +75,8 @@ class CachingOpener(urllib2.OpenerDirector):
         urllib2.OpenerDirector.__init__(self, *args, **kwargs)
 
         self.caching_dir = caching_dir
-        self.nb_calls = 0
 
         # May be cleared by the tests
         self.urls_called = []
 
-        # From urllib2.build_opener()
-        for handler in [urllib2.ProxyHandler, urllib2.UnknownHandler,
-                        urllib2.HTTPHandler, urllib2.HTTPDefaultErrorHandler,
-                        urllib2.HTTPRedirectHandler, urllib2.FTPHandler,
-                        urllib2.FileHandler, urllib2.HTTPErrorProcessor]:
-            self.add_handler(handler())
-
-    def open(self, req):
-        if isinstance(req, basestring):
-            url = req
-        else:
-            url = req.get_full_url()
-
-        self.urls_called.append(url)
-        self.nb_calls += 1
-
-        if 'LIBALBUM_ART_TEST_ONLINE' in os.environ:
-            return urllib2.OpenerDirector.open(self, url)
-
-        # XXX maybe there's a better way, using maybe an handler, to do this
-        # caching stuff...
-
-        cache_path = os.path.join(self.caching_dir,
-                                  "%d.cache" % self.nb_calls)
-
-        try:
-            # XXX not an actual urllib2.urlopen() object
-            return open(cache_path)
-        except IOError:
-            result = urllib2.OpenerDirector.open(self, url)
-            return CacheSaverResult(result, cache_path)
-
-
-class CacheSaverResult(object):
-    def __init__(self, fp, dest):
-        self.fp = fp
-        self.dest = dest
-
-    def read(self):
-        data = self.fp.read()
-        with open(self.dest, 'w') as fp:
-            fp.write(data)
-        return data
+        self.add_handler(CachingHTTPHandler(caching_dir))
